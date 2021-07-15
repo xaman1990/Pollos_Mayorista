4f47366870b483d18e993b151aa60485eaaa60e1
Alesso0908 <87213157+Alesso0908@users.noreply.github.com>
2021-07-15 16:04:46 -0500

    actualizado

--- sistema/lista_proveedor.php
+++ sistema/lista_proveedor.php
@@ -1,4 +1,52 @@
-<?php include_once "includes/header.php"; ?>
+<?php include_once "includes/header.php"; 
+
+
+$busqueda='';
+$fecha_de='';
+$fecha_a='';
+ if (!empty($_REQUEST['busqueda'])){
+   if(!is_numeric($_REQUEST['busqueda'])){
+         header("location: lista_proveedor.php");
+}
+
+$busqueda = strtolower($_REQUEST['busqueda']);
+$where="nofactura=$busqueda";
+$buscar= "busqueda=$busqueda";
+} 
+
+if(!empty($_REQUEST['fecha_de']) && !empty($_REQUEST['fecha_a'])){
+$fecha_de = $_REQUEST['fecha_de'];
+$fecha_a = $_REQUEST['fecha_a'];
+
+$buscar='';
+
+if($fecha_de > $fecha_a){
+header("location : lista_proveedores.php");
+}else if ($fecha_de == $fecha_a) {
+
+
+$where = "fecha LIKE '$fecha_de%'";
+$buscar = "fecha_de=$fecha_de&fecha_a=$fecha_a";
+
+}else{
+$f_de = $fecha_de.'00:00:00';
+$f_a = $fecha_a.'23:59:59';
+$where = "fecha BETWEEN '$f_de' AND '$f_a'";
+$buscar = "fecha_de=$fecha_de&fecha_a=$fecha_a";
+
+
+}
+}
+
+
+
+
+
+
+
+?>
+
+
 
 <!-- Begin Page Content -->
 <div class="container-fluid">
@@ -6,28 +54,24 @@
 	<!-- Page Heading -->
 	<div class="d-sm-flex align-items-center justify-content-between mb-4">
 		<h1 class="h3 mb-0 text-gray-800">Proveedores</h1>
-		<a href="registro_proveedor.php" class="btn btn-primary">Nuevo Proveedor</a>
+		<a href="registro_proveedor.php" class="btn btn-primary">Nuevo</a>
 		
 	</div>
-    <div class="row">
-	<label>Desde</label>
-      <div class="pad-md-10">
-       <input type="text" name="start_date" id="start_date" class="form-control datepicker" />
-      </div>
-	  <label >Hasta</label>
-      <div class="pad-md-5">
-       <input type="text" name="end_date" id="end_date" class="form-control datepicker" />
-      </div>      
-     </div>
-     <div class="col-md-10"> 
-      <input type="button" name="search" id="search" value="Buscar" class="btn btn-info active" />
-     </div>
-    </div>
+    <div>
+<h5> buscar por fecha </h5>
+<form action="lista_proveedor.php" method="get" class="form_search_date">
+<label>DE: </label>
+<input type="date" name="fecha_de" id="fecha_de" value="<?php echo $fecha_de;?>" required>
+<label> A </label>	
+<input type="date" name="fecha_a" id="fecha_a" value="<?php echo $fecha_a;?>" required>
+<button type="submit" class="btn_view"><i class="fas fa-search" ></i></button>
+</form>
+</div>
     <br />
-	<div class="table-responsive"  style="overflow-x: hidden;">
 	<div class="row">
 		<div class="col-lg-12">
 			<div class="table-responsive">
+					<div class="table-responsive"  style="overflow-x: hidden;">
 				<table class="table table-striped table-bordered" id="table">
 					<thead class="thead-dark">
 						<tr>
@@ -46,7 +90,7 @@
 						<?php
 						include "../conexion.php";
 
-						$query = mysqli_query($conexion, " SELECT r.codproveedor ,  p.tipoproveedor, r.proveedor , r.preciojaba, r.fechadecreacion , r.Estado FROM proveedor r INNER JOIN tipoproveedor p ON r.tipoproveedor=p.idtipoproveedor");
+						$query = mysqli_query($conexion, " SELECT  r.codproveedor ,  p.tipoproveedor, r.proveedor , r.preciojaba, r.fechadecreacion , r.Estado FROM proveedor r INNER JOIN tipoproveedor p ON r.tipoproveedor=p.idtipoproveedor");
 						$result = mysqli_num_rows($query);
 						if ($result > 0) {
 							while ($data = mysqli_fetch_assoc($query)) { ?>
